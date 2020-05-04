ec51ac2f0010f32b2d999b318a402721b8056489
Ayush Goyal <goyalayu@amazon.com>
2020-05-04 22:12:41 +0530

    Add basic list messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This Works'),
-        ),
+      body: StreamBuilder(
+          stream: Firestore.instance
+              .collection('chats/rJFhwrjmNNfhnDYxzWP7/messages')
+              .snapshots(),
+          builder: (ctx, streamSnapshot) {
+            if (streamSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            final documents = streamSnapshot.data.documents;
+            return ListView.builder(
+              itemCount: documents.length,
+              itemBuilder: (ctx, index) => Container(
+                padding: EdgeInsets.all(8),
+                child: Text(documents[index]['text']),
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
