2c97857c88b924ae1422c4d0330fc8f0827e5c0c
Ayush Goyal <goyalayu@amazon.com>
2020-05-05 21:16:02 +0530

    Send and order messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:chat_app/widgets/chat/messages.dart';
+import 'package:chat_app/widgets/chat/new_message.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -44,16 +45,9 @@ class ChatScreen extends StatelessWidget {
             Expanded(
               child: Messages(),
             ),
+            NewMessage(),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Firestore.instance
-              .collection('chats/rJFhwrjmNNfhnDYxzWP7/messages')
-              .add({'text': 'This was added by clicking the FAB!'});
-        },
       ),
     );
   }
